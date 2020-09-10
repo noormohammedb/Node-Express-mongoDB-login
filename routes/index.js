e1be477b85371f6aa9c04df37494bcf850519a10
noormohammedb <nooru.mohad@gmail.com>
2020-09-10 23:07:44 +0530

    mongodb client connection and close, hbs if for username, solved database deprecation, db connection handled properly with callback,
    signup world perfect and login getting output

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,74 @@
 var express = require('express');
-const mongodb = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 
 var router = express.Router();
-const MongoClient = mongodb.MongoClient;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  hbsObject = {title:'login page',action:'/login',button:'Login',anchar:'Signup',href:'/signup'};
+  hbsObject = {title:'login page',action:'/login',button:'Login',anchar:'Signup',href:'/signup',username:false};
   res.render('index',hbsObject);
 });
 router.get('/signup',(req, res, next)=>{
-  hbsObject = {title:'signup page',action:'/signup',button:'Signup',anchar:'login',href:'/'};
+  hbsObject = {title:'signup page',action:'/signup',button:'Signup',anchar:'login',href:'/',username:true};
   res.render('index',hbsObject)
 });
 
 router.post('/signup', (req, res, next) => {
   console.log(req.body);
 
-  MongoClient.connect('mongodb://127.0.0.1:27017', (error, client) => {
+  MongoClient.connect('mongodb://127.0.0.1:27017',{useUnifiedTopology:true}, (error, client) => {
     
     if (error) {
       console.log('DataBase Connection Error');
+      console.log(error);
+      res.send('Database Error')
     }
     else {
-      client.db('database').collection('data').insertOne(req.body);
+      client.db('express').collection('users').insertOne(req.body,(error,data)=>{
+        if(error){
+          console.log('Database insert error');
+          console.log(error);
+          res.send('database insert error');
+        }
+        else{
+          console.log('Database insert sucess');
+          console.log(data.ops);
+          hbsObject = {title:'signup sucess',user:req.body.username,done:'signup',click:true};
+          res.render('signup_or_login_sucess',hbsObject);
+        }
+        client.close();
+      });
     }
 
   });
   
-  hbsObject = {title:'signup sucess',user:req.body.username,done:'signup',click:true};
-  res.render('signup_or_login_sucess',hbsObject);
 });
 
 router.post('/login',(req,res,next) => {
+  console.log(req.body);
+
+  MongoClient.connect('mongodb://127.0.0.1:27017/',{useUnifiedTopology:true},(error,client)=>{
+    if(error){
+      console.log("DataBase Connection Error");
+      console.log(error);
+      res.send('DataBase Error');
+    }
+    else{
+      client.db('express').collection('users').find({email:req.body.email}).toArray((error,data)=>{
+        if(error){
+          console.log('error in find method');
+          console.log(error);
+          res.send('data not found');
+        }
+        else{
+          console.log('find done data is :');
+          console.log(data);
+          res.send(data);
+        }
+        client.close();
+      });
+    }
+  });
   
 });
 
